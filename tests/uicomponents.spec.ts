7e52747029171049546823c5782fa8ef5cb21e9a
DharshiniJeyachandran <dharshini.jc@gmail.com>
2025-07-15 18:53:53 +0200

    adding tests to uicomponents

--- tests/uicomponents.spec.ts
+++ tests/uicomponents.spec.ts
@@ -1,4 +1,5 @@
 import {test,expect} from '@playwright/test'
+import { UnsubscriptionError } from 'rxjs'
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200')
@@ -17,6 +18,49 @@ test('input fields', async ({page}) =>{
     await usingTheGridEmailInput.clear()
     await usingTheGridEmailInput.pressSequentially('test@test.com', {delay: 500, timeout : 10000})
 
+    //generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue()
+    expect (inputValue).toEqual('test@test.com')
+
+    //locator assertion
+    await expect (usingTheGridEmailInput).toHaveValue('test@test.com')
 
 })
+})
+
+test('radio buttons', async ({page}) => {
+   const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+   // await usingTheGridForm.getByLabel('Option 1').check({force: true})
+  await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force:true})
+  const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()
+  expect (radioStatus).toBeTruthy()
+  await expect (usingTheGridForm.getByRole('radio', {name: "Option 1"})). toBeChecked()
+
+  await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+  expect(await usingTheGridForm.getByRole('radio', {name: "Option1"}).isChecked()).toBeFalsy()
+  expect(await usingTheGridForm.getByRole('radio',{name : "Option 2"}).isChecked ()). toBeTruthy()
+
+})  
+
+
+test('radio buttons1', async ({page}) => {
+const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+  const option1Radio = usingTheGridForm.locator('nb-radio', { hasText: 'Option 1' }).locator('input[type="radio"]');
+
+})
+
+test('checkboxes', async ({page})=> {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()) {
+         await box.uncheck({force:true})
+        expect(await box.isChecked()).toBeFalsy()
+    }
 })
